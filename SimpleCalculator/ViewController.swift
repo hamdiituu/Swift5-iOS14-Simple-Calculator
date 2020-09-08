45a2c51fb9bf40966604e7490991373b17519ebd
Hamdi Tuğ <62481358+hamdiituu@users.noreply.github.com>
2020-09-08 23:22:37 +0300

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Hamdi Tuğ on 8.09.2020.
+//  Copyright © 2020 Hamdi Tuğ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
